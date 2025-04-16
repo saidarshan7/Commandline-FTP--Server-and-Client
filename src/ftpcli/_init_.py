c83d8cbb5054648aa55ee0e924e3c6dba307f9e4
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-04-16 12:16:49 +0530

    added ftp server

--- src/ftpcli/_init_.py
+++ src/ftpcli/_init_.py
@@ -1,3 +1,32 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+
+
+FTP_PORT = 2121
+
+FTP_USER = "myuser"
+
+FTP_PASSWORD = "mypassword"
+
+FTP_DIRECTORY = "."
+
+
+def main():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_DIRECTORY, perm='elradfmw')
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    handler.passive_ports = range(60000, 65535)
+
+    address = ('', FTP_PORT)
+    server = FTPServer(address, handler)
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
+
+
